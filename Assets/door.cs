96aa48670d8fe0dac01947c392412ea8122673e1
brageaasen <brageaasen@hotmail.com>
2023-04-04 19:24:29 +0200

    Finished map, added doors with functionality, made door into prefab

---
+++ Assets/door.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+
+    private bool open;
+    private Animator animator;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponentInParent<Animator>();
+        this.player = PlayerManager.instance.player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDoor()
+    {
+        this.open = true;
+        this.animator.SetTrigger("OpenDoor");
+    }
+
+    private void OnTriggerStay(Collider other) // "Press F to open" UI
+    {
+        Debug.Log("Inside");
+        if (other.tag == "Player")
+        {
+            Debug.Log("Inside");
+            if (Input.GetKeyDown(KeyCode.F)) // And player has key
+            {
+                OpenDoor();
+            }
+        }
+    }
+}
