989f3f794ae101818f7fbb4f5b3df66443496667
brageaasen <brageaasen@hotmail.com>
2023-04-04 23:44:41 +0200

    Changed enemy icon, made fader script for UI text, added begging of chests-

---
+++ Assets/Scripts/UI/Fader.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    public Text text;
+    [SerializeField] private string fadeText;
+
+    private float fadeTime;
+    private bool fadingIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.text.CrossFadeAlpha(0, 0.0f, false);
+        this.fadeTime = 0f;
+        this.fadingIn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.fadingIn)
+        {
+            FadeIn();
+        }
+        else if (this.text.color.a != 0)
+        {
+            this.text.CrossFadeAlpha(0, 0.5f, false);   
+        }
+    }
+
+    void FadeIn()
+    {
+        this.text.CrossFadeAlpha(1, 0, false);
+        this.fadeTime += Time.deltaTime;
+
+        if (this.text.color.a == 1 && this.fadeTime > 1.5f)
+        {
+            this.fadingIn = false;
+            this.fadeTime = 0f;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.fadingIn = true;
+
+            // Text to be displayed
+            this.text.text = this.fadeText;
+        }
+    }
+}
